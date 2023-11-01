e8917167182aa04570e9fefdb42432a19646acfb
webdevyoo <yjk7454@naver.com>
2023-11-01 12:31:23 +0900

    result.js 추가 - results.html 각 영역에 데이터 출력

---
+++ js/results.js
@@ -0,0 +1,23 @@
+import { results, mbtis } from "./data.js"
+
+const mbti = new URLSearchParams(location.search).get('mbti') /* location.search: url파라미터 쿼리스트링 정보 */
+const result = results[mbtis[mbti]] /* mbtis object의 key로 접근 */
+
+const titleEl = document.querySelector('.page-title')
+const characterEl = document.querySelector('.character')
+const boxEls = document.querySelectorAll('.box') /* 복수 엘리먼트 */
+const jobEls = document.querySelectorAll('.job') /* 복수 엘리먼트 */
+const lectureEl = document.querySelector('.lecture')
+const lectureImgEl = document.querySelector('.lecture img')
+
+titleEl.innerHTML = result.title
+characterEl.src = result.character
+boxEls.innerHTML = result.results[0]
+boxEls.forEach(function (obj, index) {
+  obj.innerHTML = result.results[index]
+})
+jobEls.forEach(function (obj, index) {
+  obj.innerHTML = result.jobs[index]
+})
+lectureEl.href = result.lectureUrl
+lectureImgEl.src = result.lectureImg
